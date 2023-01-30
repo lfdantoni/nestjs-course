69abd1b1834b39a20a7c213a1bef9533b29e867d
lfdantoni <lfdantoni@gmail.com>
2023-01-29 22:32:19 -0300

    test

--- src/serverless.ts
+++ src/serverless.ts
@@ -4,6 +4,8 @@ import { ValidationPipe } from '@nestjs/common';
 
 const cookieSession = require('cookie-session');
 
+let expressApp: any;
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(cookieSession({
@@ -15,7 +17,10 @@ async function bootstrap() {
       whitelist: true
     })
   );
-  const port = process.env.PORT || 3000;
-  await app.listen(port);
+
+  await app.init();
+
+  return app.getHttpAdapter().getInstance()
 }
+
 bootstrap();
